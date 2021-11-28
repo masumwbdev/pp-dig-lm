31627c32fea64c8016ff4698f6b00240afee7bbb
masum691 <masumhu691@gmail.com>
2021-11-28 19:22:12 +0600

    more style

--- src/Pages/Home/CovidTwo/CovidTwo.js
+++ src/Pages/Home/CovidTwo/CovidTwo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import covidImg from '../../../images/covid.jpg'
+import './CovidTwo.css'
 
 const CovidTwo = () => {
     return (
@@ -11,16 +12,20 @@ const CovidTwo = () => {
                         <div class="card-body">
                             <h1 class="card-title">New COVID Variant Drives U.S. Travel Bans</h1>
                             <p class="card-text fs-5">Omicron -- a new “variant of concern” -- could be more contagious, cause more severe disease and reduce the efficacy of vaccines and treatments.</p>
-                            <a href="www" alt="">New COVID-19 Cases in Kids Continue to Rise</a><br />
-                            <a href="www" alt="">5th COVID Surge Ahead Fuels 'Already Raging Fire’</a>
-                            
+                            <div className="covid-two-ancor">
+                                <a href="www" alt="">New COVID-19 Cases in Kids Continue to Rise</a>
+                            </div>
+                            <div className="covid-two-ancor">
+                                <a href="www" alt="">5th COVID Surge Ahead Fuels 'Already Raging Fire’</a>
+                            </div>
+
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card h-100 border-0">
-                            <img className="img-fluid" src={covidImg} alt="" />
-                        </div>
+                        <img className="img-fluid" src={covidImg} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
